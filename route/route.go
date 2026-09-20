6528bb9c30d7b68f3f12e8448bae390bf8b924d8
root <root@LXC-OpenCode.ret.com>
2026-09-20 13:52:13 +0800

    feat: return all portrait album images in actor info (cached after first probe)

--- route/route.go
+++ route/route.go
@@ -65,6 +65,7 @@ func New(app *engine.Engine, v auth.Validator) *gin.Engine {
 		actors := private.Group("/actors")
 		{
 			actors.GET("/:provider/:id", getInfo(app, actorInfoType))
+			actors.GET("/:provider/:id/images", getActorImages(app))
 			actors.GET("/search", getSearch(app, actorSearchType))
 			actors.POST("/batch", getActorBatch(app))
 		}
